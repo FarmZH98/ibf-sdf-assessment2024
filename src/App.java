8e1d7f04ea2b1743af87bc635a07eca88c83cc79
FarmZH98 <zhenghaofarm@gmail.com>
2024-03-01 10:51:35 +0800

    task01 completed

--- src/App.java
+++ src/App.java
@@ -1,13 +1,52 @@
 
+import java.io.Console;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class App {
 
+    public static List<List<String>> globalPokemonLists = new ArrayList<>();
+    public static Map<Integer, List<String>> globalPokemonStackMap = new HashMap<>();
     public static void main(String[] args) throws Exception {
 
         // Run Your Code here
+        String initialFileName = args[0];
+        printHeader();
+        Console cons = System.console();
+        FileService fileService = new FileService();
+        List<String> rawPokemonList = fileService.ReadCSV(initialFileName);
+        
+        //add into global pokemon list
+        for(String item : rawPokemonList) {
+            //System.out.println(item);
+            List<String> dummyList = Arrays.asList(item.split(","));
+            globalPokemonLists.add(dummyList);
+            //System.out.println(dummyList); //testing purpose
+        }
+        
+        //add into global Map - key: pokemon stack value, value: List of string containing pokemons in the stack
+        for(int i=0; i<globalPokemonLists.size(); ++i) {
+            globalPokemonStackMap.put(i+1, globalPokemonLists.get(i));
+        }
+        
 
+        String input = cons.readLine("Enter your selection >").toLowerCase();
+        if (input.equals("q")) {
+            return;
+        } else if (input.equals("4")) {
+            String newPokemonStack = cons.readLine("Create a new Pokemon stack and save to a new file >\n");
+            String fileNameToSave = cons.readLine("Enter a filename to save (e.g. path/filename.csv) >\n");
+            fileService.writeAsCSV(newPokemonStack, fileNameToSave);
+        }   
+    
+        
+        
+        
     }
 
     public static void clearConsole() throws IOException {
@@ -24,12 +63,21 @@ public class App {
     public static void printHeader() {
 
         // Task 1 - your code here
+        System.out.println("Welcome to Gaole Legend 4 Rush 2");
+        System.out.println("(1) View the list of Pokemon in the selected stack");
+        System.out.println("(2) View unique list of Pokemon in the selected stack");
+        System.out.println("(3) Find next 5 stars Pokemon occurrence");
+        System.out.println("(4) Create new Pokemon stack and save (append) to csv file");
+        
+        System.out.println("(q) to exit the program");
     }
 
     // Task 1
     public static void printExitMessage() {
 
         // Task 1 - your code here
+        System.out.println("Thank you for using the program...");
+        System.out.println("Hope to see you soon...");
     }
 
     // Task 1
--- src/FileService.java
+++ src/FileService.java
@@ -1,14 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FileService {
+    
+    //this function returns raw list, where each element contains the entire row of pokemons in csv file
+    //main program needs to handle the filtering if needed
     public List<String> ReadCSV(String fullPathFilename) {
         // Task 1 - your code here
+        List<String> pokemonList = new ArrayList<>();
 
-        return null;
+            //get csv file data and create persons
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(fullPathFilename));
+                String temp = br.readLine(); 
+
+                while(temp != null) {
+                    pokemonList.add(temp);
+                    temp = br.readLine();
+                }
+                br.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("You have provided a wrong file/path name. Please try again.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        return pokemonList;
     }
 
     public void writeAsCSV(String pokemons, String fullPathFilename) {
         // Task 1 - your code here
+        try {
+            File fileToWrite = new File(fullPathFilename);
+            if(!fileToWrite.exists()) {
+                fileToWrite.createNewFile();
+            }
 
+            FileWriter fw = new FileWriter(fileToWrite, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(pokemons + "\n");
+
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
